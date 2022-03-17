0008e4526825378cd9a0415dcdef4c2c4bcf90ea
SeWonKim <swon962@gmail.com>
2022-03-17 22:19:20 +0900

    feat: 🎸 Install and set i18n

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <Suspense fallback="loading">
+      <div className="App">
+        <MainPage />
+      </div>
+    </Suspense>
   );
 }
 
--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,17 +1,32 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import "./index.css";
 
 export default function MainPage() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="root">
       <h1>React i18n practice</h1>
-      <h2>"Hello world!"</h2>
+      <h2>{t("helloWorld")}</h2>
 
       <div className="buttonContainer">
-        <button>English</button>
-        <button>Korean</button>
-        <button>Japanese</button>
-        <button>Chinese</button>
+        <button type="button" onClick={() => changeLanguage("en")}>
+          English
+        </button>
+        <button type="button" onClick={() => changeLanguage("ko")}>
+          Korean
+        </button>
+        <button type="button" onClick={() => changeLanguage("ja")}>
+          Japanese
+        </button>
+        <button type="button" onClick={() => changeLanguage("zh")}>
+          Chinese
+        </button>
       </div>
     </div>
   );
