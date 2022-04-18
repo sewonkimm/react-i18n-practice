7caaa0267e21d192a9d82bdee9f29b21d44143c5
SeWonKim <swon962@gmail.com>
2022-04-18 14:28:17 +0900

    feat: 🎸 add locize plugin

--- src/i18n.js
+++ src/i18n.js
@@ -3,10 +3,12 @@ import { initReactI18next } from "react-i18next";
 
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import { locizePlugin } from "locize";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
+  .use(locizePlugin)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
@@ -14,6 +16,9 @@ i18n
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
+    },
+    react: {
+      bindI18n: "languageChanged editorSaved",
     },
   });
 
